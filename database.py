12e9cd4c9e91876e6774c6daf680b7f56e8d7285
Alexandria Baynes <alexandriadbaynes@gmail.com>
2021-04-24 15:42:51 -0500

    Updated database.py

--- database.py
+++ database.py
@@ -1,24 +1,89 @@
-def account_number_validation(account_number):
+# create record
+# update record
+# read record
+# delete record
+# CRUD
 
-    if account_number:
+# find user
 
-        if len(str(account_number)) == 10:
+import os
 
-            try:
-                int(account_number)
-                return True
-            except ValueError:
-                print("Invalid Account Number, account number should be integer")
-                return False
-            except TypeError:
-                print("Invalid account type")
-                return False
+user_db_path = "data/user_record/"
 
-        else:
-            print("Account Number cannot be less or more than 10 digits")
-            return False
+
+def create(account_number, user_details):
+    completion_state = False
+
+    try:
+
+        f = open(user_db_path + str(account_number) + ".txt", "x")
+
+    except FileExistsError:
+
+        print("user already exist")
+        # delete the already created file, and print out error, then return false
+        # check content of file before deleting
+        # delete(account_number)
+
+
 
     else:
-        print("Account Number is a required field")
-        return False
 
+        f.write(str(user_details))
+        completion_state = True
+
+    finally:
+
+        f.close()
+        return completion_state
+
+    # create a file
+    # name of the file would be account_number.txt
+    # add the user details to the file
+    # return true
+    # if saving to file fails, then delete created file
+
+    def read(user_account_number):
+        print("read user record")
+        # find user with account number
+        # fetch content of the file
+
+    def update(user_account_number):
+        print("update user record")
+        # find user with account number
+        # fetch the content of the file
+        # update the content of the file
+        # save the file
+        # return true
+
+
+def delete(user_account_number):
+    is_delete_successful = False
+
+    if os.path.exists(user_db_path + str(user_account_number) + ".txt"):
+
+        try:
+
+            os.remove(user_db_path + str(user_account_number) + ".txt")
+            is_delete_successful = True
+
+        except FileNotFoundError:
+
+            print("User not found")
+
+        finally:
+
+            return is_delete_successful
+
+            # find user with account number
+            # delete the user record (file)
+            # return true
+
+
+def find(user_account_number):
+    print("find user")
+    # find user record in the data folder
+
+
+create(1234567890, ["Bill", "Gate", "email@email.com", "password", 1000])
+delete()
